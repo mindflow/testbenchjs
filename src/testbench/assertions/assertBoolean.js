41193ff3ce57a460be4aa4aa22ffce522e4c0003
Mindflow <torbjorn.nilsen@gmail.com>
2020-12-28 23:03:07 +1100

    Assert Boolean.

---
+++ src/testbench/assertions/assertBoolean.js
@@ -0,0 +1,10 @@
+export class AssertBoolean {
+
+    static assertTrue(boolean = true) {
+        if(boolean) {
+            return;
+        }
+        throw "Boolean assertion failed. Expected true but was " + boolean;
+    }
+
+}
